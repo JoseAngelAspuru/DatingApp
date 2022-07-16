d4c3d81232946275295d8921ce47e6f5eb94a65f
José Angel Aspuru <josenagel01@hotamil.com>
2022-07-16 12:15:29 -0500

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -6,16 +8,18 @@ import { AccountService } from '../_services/account.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  model: any = {};
 
-  model: any = {}
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
-  constructor(public accountService: AccountService) { }
-
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   // login() {
   //   this.accountService.login(this.model).subscribe(response => {
@@ -25,12 +29,16 @@ export class NavComponent implements OnInit {
   // }
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error)
-    })
+      next: (response) => this.router.navigateByUrl('/members'),
+      error: (error) => {
+        console.log(error);
+        this.toastr.error(error.error);
+      },
+    });
   }
 
-  logout(){
+  logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,7 +12,10 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -27,9 +32,12 @@ export class RegisterComponent implements OnInit {
   // }
   register() {
     this.accountService.register(this.model).subscribe({
-      next: response => console.log(response),
-      error: error => console.log(error)      
-    })
+      next: (response) => console.log(response),
+      error: (error) => {
+        console.log(error);
+        this.toastr.error(error.error);
+      }
+    });
   }
 
   cancel() {
