1ba5083950b8f840fd0a9a9a4a5405e706adde1d
José Angel Aspuru <josenagel01@hotamil.com>
2022-07-18 19:56:07 -0500

    End of sections

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -27,14 +27,15 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
             if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
-        
+
             using var hmac = new HMACSHA512();
 
-            var user = new AppUser 
+            var user = new AppUser
             {
-                 UserName = registerDto.UserName.ToLower(),
-                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-                 PasswordSalt = hmac.Key
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+
             };
 
             _context.Users.Add(user);
@@ -50,7 +51,7 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto
+            var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto
                 .UserName.ToLower());
 
             if (user == null) return Unauthorized("invalid username");
@@ -67,14 +68,15 @@ namespace API.Controllers
             return new UserDto
             {
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
             };
         }
 
-        private async Task<bool> UserExists(string username) 
+        private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
-        
+
     }
 }
